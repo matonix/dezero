d945f10517cd6367227afd380dc7e91e4bd59e11
matonix <automatonix@gmail.com>
2021-06-14 09:02:19 +0900

    step 9 partially

--- src/main.rs
+++ src/main.rs
@@ -9,67 +9,42 @@
 // 構造は本質的に doubly linked list なので、ノードの格納は Rc<RefCell<...>> に
 // https://blog.ymgyt.io/entry/2019/08/17/013313
 // https://gist.github.com/matey-jack/3e19b6370c6f7036a9119b79a82098ca
+use ndarray::prelude::*;
 use std::cell::RefCell;
 use std::rc::Rc;
 // use std::fmt;
-
 
 fn main() {
     test_numerical_diff();
     test_forward_prop();
     test_back_prop();
-    test_back_prop_auto();
 }
 
 fn test_forward_prop() {
-    let mut f = Square::new();
-    let mut g = Exp::new();
-    let mut h = Square::new();
-    let x = Variable::new(0.5);
-    let a = f(&x);
-    let b = g(&a);
-    let y = h(&b);
+    let x = Variable::new(arr0(0.5));
+    let a = square(&x);
+    let b = exp(&a);
+    let y = square(&b);
     println!("y = {:?}", y.get_data());
 }
 
 fn test_back_prop() {
-    let mut f = Square::new();
-    let mut g = Exp::new();
-    let mut h = Square::new();
-    let x = Variable::new(0.5);
-    let a = f(&x);
-    let b = g(&a);
-    let y = h(&b);
-
-    y.set_grad(1.0);
-    b.set_grad(h.backward(y.get_grad().unwrap()));
-    a.set_grad(g.backward(b.get_grad().unwrap()));
-    x.set_grad(f.backward(a.get_grad().unwrap()));
-    println!("x.grad = {:?}", x.get_grad().unwrap());
-}
-
-fn test_back_prop_auto() {
-    let mut f = Square::new();
-    let mut g = Exp::new();
-    let mut h = Square::new();
-    let x = Variable::new(0.5);
-    let a = f(&x);
-    let b = g(&a);
-    let y = h(&b);
-
-    y.set_grad(1.0);
+    let x = Variable::new(arr0(0.5));
+    let a = square(&x);
+    let b = exp(&a);
+    let y = square(&b);
     y.backward();
     println!("x.grad = {:?}", x.get_grad().unwrap());
 }
 
 fn test_numerical_diff() {
     let mut f = Square::new();
-    let x = Variable::new(2.0);
+    let x = Variable::new(arr0(2.0));
     let dy = numerical_diff(&mut f, x, None);
     println!("dy = {:?}", dy);
 }
 
-type Data = f64;
+type Data = Array0<f64>;
 
 struct VariableCell {
     data: Data,
@@ -89,7 +64,7 @@ impl VariableCell {
             let mut funcs = vec![Rc::clone(creator)];
             while let Some(f) = funcs.pop() {
                 if let (Some(x), Some(y)) = (&f.borrow().input, &f.borrow().output) {
-                    x.borrow_mut().grad = y.borrow().grad.map(|x_| f.borrow().backward(x_));
+                    x.borrow_mut().grad = y.borrow().grad.clone().map(|x_| f.borrow().backward(x_));
                     x.borrow().creator.as_ref().map(|c| funcs.push(Rc::clone(c)));
                 } else {
                     panic!("backward: input/output of creator not found");
@@ -99,32 +74,27 @@ impl VariableCell {
     }
 }
 
-struct Variable {
+pub struct Variable {
     inner: Rc<RefCell<VariableCell>>,
 }
 
 impl Variable {
-    fn new(data: Data) -> Variable {
+    pub fn new(data: Data) -> Variable {
         Variable {
             inner: Rc::new(RefCell::new(VariableCell::new(data))),
         }
     }
-    fn clone_cell(&self) -> Rc<RefCell<VariableCell>> {
-        Rc::clone(&self.inner)
+    pub fn get_data(&self) -> Data {
+        self.inner.borrow().data.clone()
     }
-    fn get_data(&self) -> Data {
-        self.inner.borrow().data
+    pub fn get_grad(&self) -> Option<Data> {
+        self.inner.borrow().grad.clone()
     }
-    fn get_grad(&self) -> Option<Data> {
-        self.inner.borrow().grad
-    }
-    fn set_grad(&self, grad: Data) {
+    pub fn set_grad(&self, grad: Data) {
         self.inner.borrow_mut().grad = Some(grad);
     }
-    fn set_creator(&self, func: Rc<RefCell<FunctionCell>>) {
-        self.inner.borrow_mut().creator = Some(func);
-    }
-    fn backward(&self) {
+    pub fn backward(&self) {
+        self.set_grad(Array::ones(self.get_data().raw_dim()));
         self.inner.borrow().backward()
     }
 }
@@ -147,34 +117,34 @@ impl FunctionCell {
         let x = input.get_data();
         let y = forward(x);
         let output = Variable::new(y);
-        output.set_creator(func);
-        self.input = Some(input.clone_cell());
-        self.output = Some(output.clone_cell());
+        output.inner.borrow_mut().creator = Some(func);
+        self.input = Some(Rc::clone(&input.inner));
+        self.output = Some(Rc::clone(&output.inner));
         output
     }
     fn backward(&self, gy: Data) -> Data {
-        let x = self.input.as_ref().unwrap().borrow().data;
+        let x = self.input.as_ref().unwrap().borrow().data.clone();
         (self.backward)(x, gy)
     }
 }
 
-struct Square {
+pub struct Square {
     inner: Rc<RefCell<FunctionCell>>,
 }
 impl Square {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Square {
             inner: Rc::new(RefCell::new(FunctionCell::new(Self::backward_body))),
         }
     }
-    fn call(&self, input: &Variable) -> Variable {
+    pub fn call(&self, input: &Variable) -> Variable {
         self.inner.borrow_mut().cons(input, Rc::clone(&self.inner), Self::forward_body)
     }
-    fn backward(&self, gy: Data) -> Data {
+    pub fn backward(&self, gy: Data) -> Data {
         self.inner.borrow_mut().backward(gy)
     }
     fn forward_body(x: Data) -> Data {
-        x * x
+        x.map(|v| v * v)
     }
     fn backward_body(x: Data, gy: Data) -> Data {
         2.0 * x * gy
@@ -192,27 +162,31 @@ impl FnMut<(&Variable,)> for Square {
         self.call(&args.0)
     }
 }
+pub fn square(x: &Variable) -> Variable {
+    let f = Square::new();
+    f.call(x)
+}
 
-struct Exp {
+pub struct Exp {
     inner: Rc<RefCell<FunctionCell>>,
 }
 impl Exp {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Exp {
             inner: Rc::new(RefCell::new(FunctionCell::new(Self::backward_body))),
         }
     }
-    fn call(&self, input: &Variable) -> Variable {
+    pub fn call(&self, input: &Variable) -> Variable {
         self.inner.borrow_mut().cons(input, Rc::clone(&self.inner), Self::forward_body)
     }
-    fn backward(&self, gy: Data) -> Data {
+    pub fn backward(&self, gy: Data) -> Data {
         self.inner.borrow_mut().backward(gy)
     }
     fn forward_body(x: Data) -> Data {
-        x.exp()
+        x.mapv(f64::exp)
     }
     fn backward_body(x: Data, gy: Data) -> Data {
-        x.exp() * gy
+        x.mapv(f64::exp) * gy
     }
 }
 impl FnOnce<(&Variable,)> for Exp {
@@ -227,11 +201,15 @@ impl FnMut<(&Variable,)> for Exp {
         self.call(&args.0)
     }
 }
+pub fn exp(x: &Variable) -> Variable {
+    let f = Exp::new();
+    f.call(x)
+}
 
 fn numerical_diff(f: &mut Square, x: Variable, eps: Option<Data>) -> Data {
-    let e = eps.unwrap_or(1e-4);
-    let x0 = Variable::new(x.get_data() - e);
-    let x1 = Variable::new(x.get_data() + e);
+    let e = eps.unwrap_or(arr0(1e-4));
+    let x0 = Variable::new(x.get_data() - e.clone());
+    let x1 = Variable::new(x.get_data() + e.clone());
     let y0 = f(&x0);
     let y1 = f(&x1);
     (y1.get_data() - y0.get_data()) / (2.0 * e)
